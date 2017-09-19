eeece89974f3dcc3f9145c854fcabd519468c838
midoks <midoks@163.com>
2017-09-19 16:24:20 +0800

    选择一种方式

--- lib/imgproc.cpp
+++ lib/imgproc.cpp
@@ -33,10 +33,7 @@ extern "C"{
 
 
 
-
-
 zend_class_entry * opencv_imgproc_ce;
-zend_class_entry * opencv_imgproc_ce_ns;
 
 
 ZEND_BEGIN_ARG_INFO_EX(opencv_imgproc_void_arginfo, 0, 0, 0)
@@ -96,7 +93,7 @@ PHP_METHOD(opencv_imgproc, __clone) {
 /** {{{ opencv_imgproc_methods
 */
 zend_function_entry opencv_imgproc_methods[] = {
-	PHP_ME(opencv_imgproc,  __construct, opencv_imgproc_construct_arginfo,  ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+	PHP_ME(opencv_imgproc, __construct, opencv_imgproc_construct_arginfo,  ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
   PHP_ME(opencv_imgproc, __destruct,          NULL,                      ZEND_ACC_PUBLIC | ZEND_ACC_DTOR)
   PHP_ME(opencv_imgproc, __clone,             NULL,                      ZEND_ACC_PRIVATE)
   PHP_ME(opencv_imgproc, __sleep,             NULL,                      ZEND_ACC_PRIVATE)
@@ -109,15 +106,12 @@ zend_function_entry opencv_imgproc_methods[] = {
 /** {{{ OPENCV_STARTUP_FUNCTION
 */
 OPENCV_STARTUP_FUNCTION(imgproc) {
+
 	zend_class_entry ce;
-  zend_class_entry ce_ns;
-	OPENCV_INIT_CLASS_ENTRY(ce, ce_ns, "OpenCV_ImgProc", "OpenCV\\ImgProc", opencv_imgproc_methods);
+  INIT_CLASS_ENTRY(ce, "OpenCV\\ImgProc", opencv_imgproc_methods);
 
 	opencv_imgproc_ce = zend_register_internal_class_ex(&ce, NULL);
-  opencv_imgproc_ce_ns = zend_register_internal_class_ex(&ce_ns, NULL);
-
   opencv_imgproc_ce->ce_flags |= ZEND_ACC_FINAL;
-  opencv_imgproc_ce_ns->ce_flags |= ZEND_ACC_FINAL;
 
 	return SUCCESS;
 }
